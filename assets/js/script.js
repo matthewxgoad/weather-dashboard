9bee480cbc7c68eccaadbefda1cb77ebe94e3fd1
Matthew Goad <matthewxgoad@gmail.com>
2021-03-13 20:43:35 -0600

    added element ids to index; started defining global variables

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+
+// GLOBAL VARIABLES //
+
+let citiesListEl = document.getElementById('citiesListEl');
+
+let currentCityNameEL = document.getElementById('currentCityNameEl');
+let currentCityTempEL = document.getElementById('currentCityTempEL');
+let currentCityHumEL = document.getElementById('currentCityHumEL');
+let currentCityWindEL = document.getElementById('currentCityWindEL');
+let currentCityUVEL = document.getElementById('currentCityUVEL');
+
+let fiveDayCardsEl = document.getElementById('fiveDayCardsEl');
+
+
+
+
+// FUNCTIONS //
+
+
+
+
+
+
+
+// EVENT LISTENERS //
