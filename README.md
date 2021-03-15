9971ec7b3692672d3af33a1735aeb5b4a926150b
Matthew Goad <matthewxgoad@gmail.com>
2021-03-15 11:27:09 -0500

    added code comments, updated readme, updated code to allow more sidebar buttons

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# weather-dashboard
+# Weather Dashboard
+
+![image of deployed application](/assets/images/weather-dashboard-screenshot.png)
+
+[link to deployed application](http://matthewxgoad.github.com/weather-dashboard/)
+
+## Description
+
+This application allows the user to input a known city name and retreive the following weather data:
+
+* current date
+* current temperature
+* current humidity
+* current wind speed
+* current UV index
+* five day temperature (high)
+* five day humidity
+
+## Credits
+
+This application was coded by _Matthew Goad_ with the help of classmates, friends, and instructors in the Trilogy Education / Washington University coding boot camp program. 
+
+## License
+
+Use it. I'm cool.  
--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,6 @@
 let searchBtn = document.getElementById('search-btn');
 let userInput = document.getElementById('search-input');
 let citiesListEl = document.getElementById('citiesListEl');
-
 let currentCityNameEl = document.getElementById('currentCityNameEl');
 let currentDateEl = document.getElementById('currentDateEl');
 let currentCityTempEl = document.getElementById('currentCityTempEl');
@@ -12,20 +11,21 @@ let currentCityHumEl = document.getElementById('currentCityHumEl');
 let currentCityWindEl = document.getElementById('currentCityWindEl');
 let currentCityUVEl = document.getElementById('currentCityUVEl');
 let currentCityImgEl = document.getElementById('current-icon-container');
-
 let fiveDayCardsEl = document.getElementById('fiveDayCardsEl');
-
-
 
 // FUNCTIONS //
 
+// Takes user input and sends it to findLatLon function
 function processSearchBtn() {
     let city = userInput.value;
     findLatLon(city);
 }
+// Stores useer input to localStorage
 function storeCityLocal(city) {
     localStorage.setItem(city, city);
 }
+// Retrieves latitude and longitude coordinates to send to OneCall API
+// Validates that user input is a known city name
 function findLatLon(city) {
     let key = '9425b5446dc273556c5b70a438f84526';
     fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + key )
@@ -40,6 +40,9 @@ function findLatLon(city) {
         getWeather(cityLat, cityLong);
     })
 }
+// Uses lat and lon to retreive weather data and convert to json
+// Removes previously created city buttons to prevent duplicates
+// Sends weather data to insertWeatherData function
 function getWeather( lat, lon ) {
     let key = '9425b5446dc273556c5b70a438f84526';
     fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&units=imperial' + '&appid=' + key )
@@ -53,14 +56,16 @@ function getWeather( lat, lon ) {
     removeButtons();
     getStoredCities();
 }
+// Inserts weeather data to id elements on index.html
 function insertWeatherData( weather ) {
     // CURRENT DAY WEATHER DATA
     currentCityNameEl.innerHTML = userInput.value;
     currentDateEl.innerHTML = convertUnixDate(weather.current.dt);
-    currentCityTempEl.innerHTML = weather.current.temp;
-    currentCityHumEl.innerHTML = weather.current.humidity;
-    currentCityWindEl.innerHTML = weather.current.wind_speed;
+    currentCityTempEl.innerHTML = weather.current.temp + "ºF";
+    currentCityHumEl.innerHTML = weather.current.humidity + "&percnt;";
+    currentCityWindEl.innerHTML = weather.current.wind_speed + "mph";
     currentCityUVEl.innerHTML = weather.current.uvi; // update text bg color based on value
+    // changes UV element based on high, low, ok values
     if (weather.current.uvi < 3) {
         currentCityUVEl.classList.add('uvLow')
     } else if (weather.current.uvi > 9) {
@@ -93,13 +98,10 @@ function insertWeatherData( weather ) {
     document.getElementById('dayThreeHum').innerHTML = weather.daily[3].humidity;
     document.getElementById('dayFourHum').innerHTML = weather.daily[4].humidity;
     document.getElementById('dayFiveHum').innerHTML = weather.daily[5].humidity;
-}
-// Use to convert UNIX date format to Human Date Format
-function convertUnixDate (datecode) {
-    return new Date(datecode * 1000).toLocaleDateString();
-}
+}    
+// Pulls previously input city names from local and creates buttons in sidebar
 function getStoredCities() {
-    for( i = 0 ; i < localStorage.length && i < 8 ; i++) {
+    for( i = 0 ; i < localStorage.length && i < 12 ; i++) { // maximum 12 buttons
         let newListItem = document.createElement('button');
         newListItem.classList.add('btn', 'btn-success', 'btn-lg', 'btn-block');
         newListItem.innerHTML = localStorage.key(i);
@@ -109,10 +111,16 @@ function getStoredCities() {
         )
     }
 }
+// When user clicks city button in sidebar this starts the API calls again
 function processCityClick( city ) {
     userInput.value = city;
     findLatLon(city);
 }
+// Utility function to convert UNIX date format to Human Date Format
+function convertUnixDate (datecode) {
+    return new Date(datecode * 1000).toLocaleDateString();
+}
+// Utility function used to remove existing buttons and prevent duplicates
 function removeButtons() {
     while (citiesListEl.firstChild) {
         citiesListEl.removeChild(citiesListEl.firstChild);
@@ -127,15 +135,3 @@ userInput.addEventListener("keypress", function(e) {
 });
 
 document.onload = getStoredCities();
-
-
-// When user clicks buttons
-// read the city name from input
-// pass into searchSubmit fuction
-// add to localStorage
-
-// DAILY weather.daily[0-4].
-// date (converted) convertUnixDate(weather.daily[0-4].dt)
-// weather icon weather.daily[0-4].weather.icon http://openweathermap.org/img/wn/xxx.png use xxx2x.png for larger icon
-// temp weather.daily[0-4].temp
-// humidity weather.daily[0-4].humidity
